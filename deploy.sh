d845f7b9a5a2a571646e464e60bb64d553f3eb4f
Eric Zhang <ericzhang128@gmail.com>
2020-07-04 22:56:14 -0400

    rollback v1.55.1

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 echo "INSTALLING SERVERLESS FRAMEWORK"
-npm install -g serverless
+npm install -g serverless@1.55.1
 echo "DEPLOYING SERVERLESS"
 serverless deploy --stage $env --package $CODEBUILD_SRC_DIR/target/$env -v -r us-east-1
