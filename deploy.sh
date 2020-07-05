e2bfc65c0ad0ec9f32c34fdc5af8f1a495992067
Eric Zhang <ericzhang128@gmail.com>
2020-07-04 23:37:34 -0400

    updated code

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
 
-echo "INSTALLING SERVERLESS FRAMEWORK"
+echo "Installing serverless"
+echo "====================="
 npm uninstall -g serverless
 npm install -g serverless
-echo "DEPLOYING SERVERLESS"
+npm install serverless-offline serverless-stack-output 
+
+echo "Deploying app to $env"
+echo "====================="
 serverless deploy --stage $env --package $CODEBUILD_SRC_DIR/target/$env -v -r us-east-1
